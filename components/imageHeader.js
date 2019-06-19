ffd1fc19970b0d61e4eef8722966b3c67877aa52
Michael Campbell <mcampbell0918@gmail.com>
2019-06-19 16:04:07 -0500

    started image component

---
+++ components/imageHeader.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import { string } from 'prop-types';
+
+
+const imageHeader = (props) => (
+    <div>yee</div>
+);
+
+imageHeader.propTypes = {
+    image: string
+};
+
+export default imageHeader;
